f3f3408a14e74a8aba342f3605917e8d54ab8396
Ghazaleh Ansari <gansari@c3a4c7.42berlin.de>
2025-03-18 17:54:02 +0100

    fix fork choice

--- srcs/init.c
+++ srcs/init.c
@@ -66,10 +66,7 @@ bool	init_philos(t_data *data)
 		data->philos[i].last_meal_time = data->start_time;
 		data->philos[i].data = data;
 		data->philos[i].left_fork = &data->forks[i];
-		if (i == 0)
-			data->philos[i].right_fork = &data->forks[data->num_philos - 1];
-		else
-			data->philos[i].right_fork = &data->forks[(i + 1) % data->num_philos];
+		data->philos[i].right_fork = &data->forks[(i + 1) % data->num_philos];
 		if (pthread_mutex_init(&data->philos[i].meal_mutex, NULL) != 0)
 		{
 			while (--i >= 0)
